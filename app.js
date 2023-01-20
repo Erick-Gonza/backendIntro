edfef437104fb0a080c05ec75f5a7fb183b20847
erickGzz <eagm080897@gmail.com>
2023-01-20 16:44:56 -0600

    chat app with webscokets

--- app.js
+++ app.js
@@ -1,37 +1,38 @@
 import express from 'express'
 import { createServer } from 'http'
 import { Server } from 'socket.io'
-import path from 'path'
 
 const port = 3000
 const app = express()
 const httpServer = createServer(app)
 const io = new Server(httpServer)
 
-const __dirname = path.resolve()
+let messages = [{ user: 'admin', text: 'Welcome to the chat' }]
 
 //Server
 io.on('connection', (socket) => {
-  //Interact with the client || socket
   const userInstances = io.engine.clientsCount
+  //Interact with the client || socket
   console.log(`user connected: ${socket.id} - ${userInstances} users connected`)
+  socket.emit('message', messages)
 
-  socket.emit('message', 'Hello from the server')
+  //Listen to the client || socket
+  socket.on('newMessage', (message) => {
+    messages.push(message)
+    io.sockets.emit('message', messages)
+  })
 
   socket.on('disconnect', () => {
     console.log(`user disconnected: ${socket.id}`)
-  })
-  socket.on('answer', (message) => {
-    console.log(message)
   })
 })
 
 app.use(express.static('./public'))
 
 app.get('/', (req, res) => {
-  res.sendFile(`${__dirname}/index.html`)
+  res.sendFile(`index.html`)
 })
 
-httpServer.listen(port, () => {
-  console.log(`listening on port: ${port}`)
+httpServer.listen(port, '192.168.50.135', () => {
+  console.log(`Server running on port ${port}`)
 })
